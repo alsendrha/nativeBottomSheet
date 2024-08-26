a01ac8da0636520764dc8fc67901b380f22632e4
Minyoung <alsendrha@naver.com>
2024-08-26 16:27:23 +0900

    옵션 조정

--- src/components/MyBottomSheet.tsx
+++ src/components/MyBottomSheet.tsx
@@ -5,7 +5,18 @@ const MyBottomSheet = ({refBottomSheet}: any) => {
   const snapPoints = useMemo(() => ['25%', '50%', '70%', '100%'], []);
   return (
     <View style={styles.container}>
-      <BottomSheet ref={refBottomSheet} index={1} snapPoints={snapPoints}>
+      <BottomSheet
+        ref={refBottomSheet}
+        index={1}
+        snapPoints={snapPoints}
+        // 높이 지정
+        enablePanDownToClose={true}
+        // 드래그로 닫기 활성화
+        handleIndicatorStyle={{backgroundColor: 'red'}}
+        // 핸들 스타일 지정
+        backgroundStyle={{backgroundColor: 'rgba(0,0,0,0.5)'}}
+        // 배경 스타일 지정
+      >
         <View style={styles.contentContainer}>
           <Text style={styles.containerHeadline}>Awesome Bottom Sheet 🎉</Text>
         </View>
